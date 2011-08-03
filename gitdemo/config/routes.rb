21a00ddb9f02a3dcdc0f86576a9efd05fae74b0d
wangshuai427 <244580440@qq.com>
2011-08-03 10:44:56 +0800

    gitdemo

--- gitdemo/config/routes.rb
+++ gitdemo/config/routes.rb
@@ -1,26 +1,21 @@
-Depot::Application.routes.draw do 
-  get 'admin'=>'admin#index'
+#---
+# Excerpted from "Agile Web Development with Rails, 4rd Ed.",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
+Depot::Application.routes.draw do
+  resources :line_items
 
-  controller :sessions do
-    get  'login'=>:new
-    post 'login'=>:create
-    delete 'logout'=>:destroy
-  end
-  
-  scope '(:locale)' do
-    resources :users
-    resources :orders
-    resources :line_items
-    resources :carts
-    resources :products do
-      get :who_bought,:on=>:member
-    end
-    root :to=> 'store#index', :as=>'store'
-  end
-  
-  
+  resources :carts
 
-  
+  get "store/index"
+
+  resources :products
+
+  # ...
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -71,7 +66,10 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-  
+  root :to => 'store#index', :as => 'store'
+
+  # ...
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
